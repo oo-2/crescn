2349bcbf800aa48ea3d8987f573daadd9a661015
oo-2 <79114582+oo-2@users.noreply.github.com>
2023-06-01 23:50:22 -0500

    cleaned audio controller

--- server/controllers/audioController.js
+++ server/controllers/audioController.js
@@ -2,33 +2,30 @@ const YoutubeMusicApi = require("youtube-music-api");
 const ytdl = require("ytdl-core");
 
 const getAudio = async (req, res) => {
-
   const music = new YoutubeMusicApi();
-  var url = "";
+  var id = "";
+  const options = {
+    format: "mp3",
+    filter: "audioonly",
+  };
   await music.initalize();
   await music.search(req.params.query, "song").then((result) => {
     if (Object.values(result.content).length < 1) {
       res.status(404).json({ error: "Audio not found" });
     }
     var song = result.content[0];
-    url = `http://www.youtube.com/watch?v=${song.videoId}`;
+    id = song.videoId;
   });
 
   try {
-    let info = await ytdl.getInfo(url);
-    const meta = ytdl.chooseFormat(info.formats, {
-      format: "mp3",
-      filter: "audioonly",
-    });
+    let info = await ytdl.getInfo(id);
+    const meta = ytdl.chooseFormat(info.formats);
     res.set({
       "Content-Type": "audio/mp3",
       "Content-Length": meta.contentLength,
       "Accept-Ranges": `bytes 0-${meta.contentLength}`,
     });
-    const stream = ytdl(url, {
-      format: "mp3",
-      filter: "audioonly",
-    }).pipe(res);
+    const stream = ytdl(id, options).pipe(res);
 
     stream.on("error", (err) => {
       console.error("Error occurred during streaming:", err);
