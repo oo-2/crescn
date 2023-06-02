178a0937417859248ebd29a2741004fad0650ea7
oo-2 <79114582+oo-2@users.noreply.github.com>
2023-06-01 22:42:18 -0500

    updated get audio

--- server/controllers/audioController.js
+++ server/controllers/audioController.js
@@ -1,65 +1,54 @@
 const YoutubeMusicApi = require("youtube-music-api");
 const ytdl = require("ytdl-core");
 
+
+
+
+
 const getAudio = async (req, res) => {
   const music = new YoutubeMusicApi();
-  var url = "";
+  var videoId = "";
   await music.initalize();
   await music.search(req.params.query, "song").then((result) => {
     if (Object.values(result.content).length < 1) {
       res.status(404).json({ error: "Audio not found" });
     }
     var song = result.content[0];
-    url = `http://www.youtube.com/watch?v=${song.videoId}`;
+    videoId = song.videoId;
   });
 
   try {
-    let info = await ytdl.getInfo(url);
-    const meta = ytdl.chooseFormat(info.formats, {
+    const range = req.headers.range;
+    const options = {
       format: "mp3",
       filter: "audioonly",
-    });
-    
-    const range = req.headers.range;
+    };
+    let info = await ytdl.getInfo(videoId);
+    const meta = ytdl.chooseFormat(info.formats, options);
+    let stream = ytdl(videoId, options);
     const fileSize = meta.contentLength;
-    
+    res.set("Content-Length", fileSize);
     if (range) {
       const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0]);
-      const end = parts[1] ? parseInt(parts[1]) : fileSize - 1;
-    
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
       const chunkSize = end - start + 1;
-      const stream = ytdl(url, {
+      stream = ytdl(videoId, {
         format: "mp3",
         filter: "audioonly",
-      
+        range: {start: start, end: end}
       });
-    
-      res.writeHead(206, {
-        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunkSize,
-        "Content-Type": "audio/mp3",
-      });
-    
-      stream.pipe(res);
-    } else {
-      res.set({
-        "Content-Type": "audio/mp3",
-        "Content-Length": fileSize,
-        "Accept-Ranges": "bytes",
-      });
-    
-      const stream = ytdl(url, {
-        format: "mp3",
-        filter: "audioonly",
-      });
-    
-      stream.pipe(res);
+      res.set("Content-Length", chunkSize);
+      res.set("Content-Range", `bytes ${start}-${end}/${fileSize}`);
     }
+    res.writeHead(200, {
+      "Accept-Ranges": "bytes",
+      "Content-Type": "audio/mp3",
+    });
+    stream.pipe(res);
   } catch (err) {
-    console.error("Error occurred while getting video info:", err);
-    res.status(500).send("An error occurred while getting video info.");
+    console.error("Error occurred while getting audio info:", err);
+    res.status(500).send("An error occurred while getting audio info.");
   }
 };
 
