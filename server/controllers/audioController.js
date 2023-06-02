2dc0da936bcbfef3225a0733d9b278567d548993
oo-2 <79114582+oo-2@users.noreply.github.com>
2023-06-01 23:49:57 -0500

    reverting audio controller

--- server/controllers/audioController.js
+++ server/controllers/audioController.js
@@ -1,54 +1,39 @@
 const YoutubeMusicApi = require("youtube-music-api");
 const ytdl = require("ytdl-core");
 
-
-
-
-
 const getAudio = async (req, res) => {
   const music = new YoutubeMusicApi();
-  var videoId = "";
+  var id = "";
+  const options = {
+    format: "mp3",
+    filter: "audioonly",
+  };
   await music.initalize();
   await music.search(req.params.query, "song").then((result) => {
     if (Object.values(result.content).length < 1) {
       res.status(404).json({ error: "Audio not found" });
     }
     var song = result.content[0];
-    videoId = song.videoId;
+    id = song.videoId;
   });
 
   try {
-    const range = req.headers.range;
-    const options = {
-      format: "mp3",
-      filter: "audioonly",
-    };
-    let info = await ytdl.getInfo(videoId);
-    const meta = ytdl.chooseFormat(info.formats, options);
-    let stream = ytdl(videoId, options);
-    const fileSize = meta.contentLength;
-    res.set("Content-Length", fileSize);
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-      const chunkSize = end - start + 1;
-      stream = ytdl(videoId, {
-        format: "mp3",
-        filter: "audioonly",
-        range: {start: start, end: end}
-      });
-      res.set("Content-Length", chunkSize);
-      res.set("Content-Range", `bytes ${start}-${end}/${fileSize}`);
-    }
-    res.writeHead(200, {
-      "Accept-Ranges": "bytes",
+    let info = await ytdl.getInfo(id);
+    const meta = ytdl.chooseFormat(info.formats);
+    res.set({
       "Content-Type": "audio/mp3",
+      "Content-Length": meta.contentLength,
+      "Accept-Ranges": `bytes 0-${meta.contentLength}`,
     });
-    stream.pipe(res);
+    const stream = ytdl(id, options).pipe(res);
+
+    stream.on("error", (err) => {
+      console.error("Error occurred during streaming:", err);
+      res.status(500).send("An error occurred during streaming.");
+    });
   } catch (err) {
-    console.error("Error occurred while getting audio info:", err);
-    res.status(500).send("An error occurred while getting audio info.");
+    console.error("Error occurred while getting video info:", err);
+    res.status(500).send("An error occurred while getting video info.");
   }
 };
 
