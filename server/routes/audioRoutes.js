e7b336dbe2a6df916be387189792333e7483c66b
oo-2 <79114582+oo-2@users.noreply.github.com>
2023-06-03 23:09:13 -0500

    Updated audio, decreased audio mismatches

--- server/routes/audioRoutes.js
+++ server/routes/audioRoutes.js
@@ -2,6 +2,9 @@ const { getAudio } = require("../controllers/audioController");
 const express = require("express");
 const router = express.Router();
 
-router.get("/audio/:query", async (req, res) => await getAudio(req, res));
+router.get(
+  "/audio/:artist/:track/:duration",
+  async (req, res) => await getAudio(req, res)
+);
 
 module.exports = router;
