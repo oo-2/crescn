da55cfb021c8f3d6a6966dfdd1bd2ac6fc2b1602
oo-2 <79114582+oo-2@users.noreply.github.com>
2023-06-03 23:09:46 -0500

    removed an audioroute

--- server/routes/audioRoutes.js
+++ server/routes/audioRoutes.js
@@ -1,14 +1,10 @@
-const { getAudio, searchAudio } = require("../controllers/audioController");
+const { getAudio } = require("../controllers/audioController");
 const express = require("express");
 const router = express.Router();
 
 router.get(
   "/audio/:artist/:track/:duration",
   async (req, res) => await getAudio(req, res)
-);
-router.get(
-  "/audio/search/:query",
-  async (req, res) => await searchAudio(req, res)
 );
 
 module.exports = router;
